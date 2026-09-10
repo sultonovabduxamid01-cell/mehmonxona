43d7e18fdb807fb6c18861e711b6a52ad73d6752
Abdulhamid <sultonovabduxamid01@gmail.com>
2026-09-10 17:31:02 +0500

    Add requirements

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e8d53w-!6^k0_n+m(th_#shcd-n^3y_=p%h5a&nt0h$)wwv*3$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
